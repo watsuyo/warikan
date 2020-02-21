3d07081ee0c96b0f4014fcc84169b7a50c1c851c
Tsuyoshi Hirosawa <dmwtpdmwtp24@gmail.com>
2020-02-22 00:46:13 +0900

    chore: lint (#1)

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,21 +10,28 @@ module.exports = {
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended',
-    '@nuxtjs/eslint-config-typescript'
+    '@nuxtjs/eslint-config-typescript',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:vue/recommended'
   ],
   plugins: [
     '@typescript-eslint'
   ],
+  plugins: ['@typescript-eslint'],
   // add your custom rules here
   rules: {
     semi: [2, 'never'],
     'vue/html-indent': ['error', 2],
-    'vue/html-self-closing': 'off',//imgタグのようにタグが１つで完結してもOK
+    'vue/html-self-closing': 'off', //imgタグのようにタグが１つで完結してもOK
     'vue/max-attributes-per-line': 'off',
-    'space-before-function-paren': ['off', {
-      anonymous: 'always',
-      named: 'never',
-      asyncArrow: 'always'
-    }],
+    'space-before-function-paren': [
+      'warn',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ]
   }
 }
